4adf94d9657c68d0ac2b78f182f1685b010ba118
Dennis <d.ochse@open2bizz.nl>
2019-05-02 10:45:53 +0200

    Add important notes to the repo, change the license to LGPL

--- orbeon/controllers/orbeon.py
+++ orbeon/controllers/orbeon.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    Copyright (c) 2016 - Open2bizz
-#    Author: Open2bizz
+# Author: Open2Bizz (www.open2bizz.nl)
+# Employee: Dennis Ochse
+# Date: 2019-05-02
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# GNU LESSER GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
 #
-#    A copy of the GNU General Public License is available at:
-#    <http://www.gnu.org/licenses/gpl.html>.
+#
+# This version of the GNU Lesser General Public License incorporates
+# the terms and conditions of version 3 of the GNU General Public
+# License, supplemented by the additional permissions listed in the following URL:
+# https://www.gnu.org/licenses/lgpl.txt.
 #
 ##############################################################################
 import requests
